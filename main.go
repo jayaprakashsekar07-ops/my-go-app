444aaa7e5e7c6fa2a84d817ca35081114f049bc6
jayaprakashsekar07-ops <jayaprakashsekar07@gmail.com>
2026-02-06 06:23:00 +0530

    new change

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello! My Git journey starts now 🚀")
+	fmt.Println("Hello! My Git journey starts now 🚀")
 
-	fmt.Println("Hellooooooooo")
+	// fmt.Println("Hellooooooooo")
+	store := make(map[int]int)
+	var numbers = []int{10, 20, 30, 40, 50, 60, 70}
+	for index, val := range numbers {
+		fmt.Println(store[index] == val)
+	}
 }
